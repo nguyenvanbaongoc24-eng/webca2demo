0dbcfad36cc50f36d2613dc17a305da5decc22ce
nguyenvanbaongoc24-eng <nguyenvan.baongoc24@gmail.com>
2026-05-15 17:09:08 +0700

    fix: add missing User icon import in Header

--- nacencomm-premium/src/components/sections/Header.tsx
+++ nacencomm-premium/src/components/sections/Header.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { User } from "lucide-react";
 import { Button } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
 
